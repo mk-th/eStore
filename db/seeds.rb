c167f67250111c0c3f37fe94ec15e18566f70826
Manish <manishthorani@gmail.com>
2014-05-05 04:13:09 +0500

    Seeds For first user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create!(:name => 'Manish', :email => 'manishthorani@gmail.com', :password => 'Manish.7')
